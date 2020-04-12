221adc42286d1d57fcab95c4daf0cdf8d0c9d9f5
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-04-12 14:07:12 -0700

    Create class.dart

---
+++ OOP Review/class.dart
@@ -0,0 +1,10 @@
+// When we are working with classes, we should name it with the initial letter being a Capital letter.
+//Then we are defining its property/members and methods, we should use the cammelcase convention.
+
+class Car{
+  int numberOfDoors = 5;
+  
+  void drive(){
+    print("Wheels start turning!");
+  }
+};
