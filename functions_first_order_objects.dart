733bff11dc3c3d0d23aa1e4a945d40a515ba4ef2
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-04-20 16:00:25 -0700

    Create functions_first_order_objects.dart

---
+++ functions_first_order_objects.dart
@@ -0,0 +1,27 @@
+// Dart apresenta uma particularidade em relacao a outras linguagens de programacao. Essa particularidade eh que funcoes podem ser usadas
+// como objetos de primeira ordem. Oq isso quer dizer?? Que as funcoes em Dart funcionam exatamente como qualquer outro tipo de objeto. Ou
+// seja, podemos armazenar funcoes dentro de variaveis e alem disso podemos passar funcoes como parametro de outras funcoes. Tal vantagem
+// de Dart, permite que tenhamos grande flexibilidade quando trabalhando com funcoes.
+// Em Dart, o tipo que representa funcoes eh o tipo "Function".
+
+
+void main(){
+  
+  int result = calculator(4, 25, multiply);
+  print(result);
+  
+  return;
+}
+
+int calculator(int n1, int n2, Function calculation){
+  return calculation(n1, n2);
+}
+
+
+int add(int n1, int n2){
+  return n1 + n2;
+}
+
+int multiply(int n1, int n2){
+  return n1*n2;
+}
