aaf1cbe330fb082df217de088a0a318dc2805218
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-05-01 16:59:24 -0700

    Create loops1.dart

---
+++ FOR LOOPS/loops1.dart
@@ -0,0 +1,10 @@
+
+void main(){
+  for(int i = 99; i >= 2; i--){
+    print('$i bottles of beer on the wall, $i bottles of beer.');
+    print('Take one down and pass it around, ${i - 1} bottles of beer on the wall.');
+    print('');
+  }
+  
+  return;
+}
