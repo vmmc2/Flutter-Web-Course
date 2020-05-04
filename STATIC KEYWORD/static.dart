6ffd186bb585c699b40832e6e55d6111cd2a4ec8
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-05-04 08:22:59 -0700

    Update static.dart

--- STATIC KEYWORD/static.dart
+++ STATIC KEYWORD/static.dart
@@ -15,3 +15,8 @@ print(Car.id);
 
 //Oq eh interessante a respeito do uso da "static" keyword eh que a gente pode ir fazendo modificacoes nessa variavel "static" por meio da classe.
 //Entretanto, nao da pra ter acesso a variaveis "static" por meio de uma instancia da classe (objeto).
+
+//Em outras palavras, quando a gente declara um variavel dentro de uma class sem usar a keyword "static" estamos criando uma variavel de instancia.
+//Uma instance variable. Que pode ser modificada apenas em instancias/objetos.
+//Agora quando a gente declara uma variavel usando a keyword "static", a gente ta criando uma variavel de classe. Ou, em outras palavras, uma class-wide variable.
+//Esse tipo de variavel so pode ser acessado e alterado por meio de uma classe. Nao pode ser alterado/acessado/modificado por meio de uma instancia.
