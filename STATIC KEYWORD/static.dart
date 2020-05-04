1c90d34b93ca79f4ffdb3fc6c230ca094aecc4b8
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-05-04 07:53:47 -0700

    Update static.dart

--- STATIC KEYWORD/static.dart
+++ STATIC KEYWORD/static.dart
@@ -1,1 +1,13 @@
 //Basicamente a keyword "static" quando usada em variaveis dentro de uma classe serve para dizer que essa variavel existe dentro da classe. Sendo assim, eu nao preciso criar um objeto/instancia para ter acesso a essa variavel.
+//Por exemplo, vamos supor que eu tenho uma classe chamada Car. Observe o exemplo abaixo:
+class Car{
+  static String id = "meu_carro";
+  String name = "dale";
+  
+  Car({this.name});
+}
+
+//Para acessar o parametro "name", eu preciso criar um objeto/instancia dessa classe.
+//Mas para acessar o parametro "id", eu nao preciso criar um objeto/instancia da classe. Por que essa property "vive" dentro da classe com o valor: "meu_carro".
+//Portanto, para acessar tal property, basta eu codar o seguinte:
+print(Car.id);
