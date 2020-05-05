16250a1ed38526c5f012e6783d68045b0ba92aeb
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-05-05 13:35:49 -0700

    Update mixins.dart

--- mixins.dart
+++ mixins.dart
@@ -1,5 +1,6 @@
 //First of all, what are mixins?
 //Mixins are just a way of reusing a class's code in multiple class hierarchies.
+//Or, in a simple way, Mixin is a tool that enable us to add features to a Class.
 //To use a mixin, use the "with" keyword followed by one or more mixin names. The following examples shows two classes that use mixins:
 
 class Musician extends Performer with Musical{
